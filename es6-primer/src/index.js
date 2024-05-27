f628f1aa679ff1e9b3d3c86a4f77f0a829b55dd1
Mirireoluwa Olukanni <mirireoluwa.olukanni@pau.edu.ng>
2024-05-27 16:04:32 +0100

    JavaScript stuff

---
+++ es6-primer/src/index.js
@@ -0,0 +1,13 @@
+import log from './tools'
+log("Welcome to Chapter 10! We are noe having fun with modularity");
+
+
+//import log, {Person} from './tools';
+//import log, {User} from './tools';
+
+/*let person1 = new Person("Mirireoluwa", "Olukanni", "Male", 1.7);
+let person2 = new Person("Mary", "Joseph", "Female", undefined);
+let user1 = new User("myusername", "mypassword", "Mirireoluwa", "Onobhayedo", "Male", undefined);
+  d
+log(`Person 1 is${person1.firstName} whose height is ${person1.height}. Person 2 is ${person2.firstName} whose height is ${person2.height}`);
+log(`The username of ${user1.firstName} is ${user1.username}`);*/
---
+++ es6-primer/src/tools.js
@@ -0,0 +1,24 @@
+var log = function(output)
+{
+  document.getElementById("output").innerHTML = output;
+};
+
+export default log;
+/*export class Person{
+  constructor(firstName, lastName, gender, height){
+    this.firstName = firstName;
+    this,lastName = lastName;
+    this.gender = gender;
+    this.height = height;
+  }
+};*/
+
+
+
+/*export class User extends Person {
+  constructor(username, password, firstName, lastName, gender, height){
+    super(firstName, lastName, gender, height);
+    this.username = username;
+    this.password = password;
+  }
+};*/
